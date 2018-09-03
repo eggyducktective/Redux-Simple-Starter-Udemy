4d5b9cbd16bf51ff956c969e470a9145d9ad3ab1
Anna Tran <anhkaehne@gmail.com>
2018-09-03 19:20:31 +1000

    added video lists and video details

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyBhwuU9Aq17HL5o3WxdlO8jIzKKZH6R8vQ';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render( <App />, document.querySelector('.container'));
